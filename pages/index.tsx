3e424dbac6eaf91141899e19931256d50d5c4976
Prakash Kalsariya <pralashkalsariya1999@gmail.com>
2022-03-07 17:11:47 +0530

    changes for banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.scss";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -13,38 +13,124 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     <div className={styles.main_layout_container}>
-       
-       <header className={styles.header}></header>
-       
-       <nav className={styles.navbar}>
-         
-            <div className={styles.logo}>
-              <ul><li><Link href="/"><a>Aam Health</a></Link></li></ul>
+      <div className={styles.main_layout_container}>
+        <header className={styles.header}>
+        <nav className={styles.navbar}>
+          <div className={styles.logo}>
+            <ul>
+              <li>
+                <Link href="/">
+                  <a>
+                    <h1>Aam Health</h1>
+                  </a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.menubar}>
+            <ul>
+              <li>
+                <Link href="">
+                  <a>COVID-19</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="">
+                  <a>Get Care</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="">
+                  <a>Insurance & Pricing</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="">
+                  <a>Location </a>
+                </Link>
+              </li>
+              <li>
+                <Link href="">
+                  <a>Company</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className={styles.login_btn}>
+            <button>Log In</button>
+          </div>
+        </nav>
+        </header>
+
+
+        <div className={styles.banner_container}>
+          <div className={styles.banner_img}>
+            <Image
+              src="/images/banner.jpg"
+              alt="banner"
+              width={1600}
+              height={920}
+            />
+          </div>
+
+          <div className={styles.banner_contain_container}>
+            <div className={styles.contain_text_area_container}>
+              <h1>
+                Making good health accessible <span>to all.</span>{" "}
+              </h1>
+              <p>Book same day appointments. No subscriptions.</p>
             </div>
-              <div className={styles.menubar}>
-               <ul>
-                 <li><Link href=""><a>COVID-19</a></Link></li>
-                 <li><Link href=""><a>Get Care</a></Link></li>
-                 <li><Link href=""><a>Insurance & Pricing</a></Link></li>
-                 <li><Link href=""><a>Location </a></Link></li>
-                 <li><Link href=""><a>Company</a></Link></li>
-               </ul>
-             </div>
 
-             <div className='login_btn'>
-               <button>Log In</button>
-             </div>
+            <div className={styles.contain_box_container}>
+              <div className={styles.box_heading}>
+                <p>Book an Appointment</p>
+              </div>
 
+              <div className={styles.box_item_container}>
+                <div className={styles.box_primary_care_container}>
+                  <div className={styles.box_circle}> </div>
+                  <div className={styles.box_contain}>
+                    <h3>Primary Care</h3>
+                    <p>Ideal for preventive Care </p>
+                  </div>
+                  <div className={styles.box_date}>
+                    <p>Next: Today 10pm</p>
+                  </div>
+                </div>
 
+                <div className={styles.box_urgent_care_container}>
+                  <div className={styles.box_circle}> </div>
+                  <div className={styles.box_contain}>
+                    <h3>Urgent Care</h3>
+                    <p>Ideal for immediate medical attention</p>
+                  </div>
+                  <div className={styles.box_date}>
+                    <p>Next: Today 6pm</p>
+                  </div>
+                </div>
 
-         
-       </nav>
-      
-       
-       </div>
+                <div className={styles.box_virtual_care_container}>
+                  <div className={styles.box_circle}> </div>
+                  <div className={styles.box_contain}>
+                    <h3>Virtual Care</h3>
+                    <p>Video with your health team</p>
+                  </div>
+                  <div className={styles.box_date}>
+                    <p>Next: Today 7pm</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className={styles.box_btn}>
+                <button>Book In Appointment</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
