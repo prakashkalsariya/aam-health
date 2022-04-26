87a7161f7dd64f4e2e02fac44e87fced9fa5d483
Niten Solanki <nitensolanki02@gmail.com>
2022-04-26 09:51:16 +0530

    Sample data for investors

---
+++ data/investors.ts
@@ -0,0 +1,11 @@
+export const investorsList = [
+  {
+    first_name: "John",
+    last_name: "Doe",
+    email: "johndoe@hotmail.com",
+    phone: "+91 4556565652",
+    amount: 50000,
+    image: "",
+    created_at: new Date().toISOString(),
+  },
+];
